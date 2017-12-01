94fb4f3290fa1bc2b6a2483eda3ab703c2da6efc
Shahidn3m0 <34151855+Shahidn3m0@users.noreply.github.com>
2017-12-01 13:25:15 +0700

    Update unfollownotfollowback.js

--- unfollownotfollowback.js
+++ unfollownotfollowback.js
@@ -10,8 +10,8 @@ const delay = require('delay');
 const _ = require('lodash');
 
 const User = {
-    username: '', // Your Username Instagram
-    password: '', // Your Password Instagram
+    username: 'shahidn3', // Your Username Instagram
+    password: 'shahid020599', // Your Password Instagram
 }
 
 const doLogin = async function(User){
